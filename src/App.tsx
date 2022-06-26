f1a16becf64266aa61b0b994afda176ddadc6d6c
Zach Nugent <zachary.nugent@usewalter.com>
2022-06-26 17:58:16 -0400

    style 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,16 @@ const App: React.FC = () => {
       <Routes>
         <Route path='/' element={<Homepage />} />
         <Route path='/guest-book' element={<GuestBook />} />
-        <Route element={<div>404 Page Not Found</div>} />
+        <Route
+          path='*'
+          element={
+            <div className='h-screen flex items-center justify-center'>
+              <h1 className='text-5xl font-bold text-primary-content'>
+                404 Page Not Found
+              </h1>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
